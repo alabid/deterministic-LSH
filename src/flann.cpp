ee5d2c7bd42a71671d00c57b14ad954789142c36
Daniel Alabi <alabidan@gmail.com>
2015-12-06 18:53:24 -0500

    Don't rely on FLANN hdfs5

--- src/flann.cpp
+++ src/flann.cpp
@@ -2,24 +2,51 @@
  * Flann LSH
  */
 
-#include <flann/flann.hpp>
-#include <flann/io/hdf5.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-#include <stdio.h>
+#include <flann/flann.hpp>
 
 using namespace flann;
+using namespace std;
 
-int main(int argc, char** argv)
-{
-    int nn = 3;
+void load_from_file(Matrix<float>& dataset, const string& filename) {
+  ifstream dfile(filename);
+  if (!dfile.is_open()) {
+    cout << "unable to open data set file: " << filename << endl;
+    exit(1);
+  }
 
+  string line;
+  int pointNum = 0;
+  cerr << "before reading first line" << endl;
+  while (getline(dfile, line)) {
+    cerr << line << endl;
+    stringstream ss(line);
+    int i = 0;
+    int j = 0;
+    char c;
+    while(ss >> c) {
+      cerr << "c: " << c;
+      dataset[i][j++] = c-'0';
+      i++;
+    }
+  }
+  cout << "came to the end" << endl;
+  dfile.close();
+}
+
+int main(int argc, char** argv) {
     Matrix<float> dataset;
     Matrix<float> query;
-    load_from_file(dataset, "data/files/dataset.hdf5","dataset");
-    load_from_file(query, "data/files/dataset.hdf5","query");
+    load_from_file(dataset, "data/files/d10nd16");
+    load_from_file(query, "data/files/d10nq4");
 
-    Matrix<int> indices(new int[query.rows*nn], query.rows, nn);
-    Matrix<float> dists(new float[query.rows*nn], query.rows, nn);
+    int d = 10;
+    Matrix<int> indices(new int[query.rows*d], query.rows, d);
+    Matrix<float> dists(new float[query.rows*d], query.rows, d);
 
     // construct a hierarchical clustering index
     // default paramaters:
@@ -27,14 +54,13 @@ int main(int argc, char** argv)
     // centers: random
     // number of parallel trees: 4
     // leaf_max_size: 100
-    Index<L2<float> > index(dataset, flann::HierarchicalClusteringIndexParams());
+    Index<L2<float>> index(dataset, flann::HierarchicalClusteringIndexParams());
     index.buildIndex();
 
-    // do a knn search, using 128 checks
+    // do a radius search, using 128 checks
     // checks : specifies the maximum leafs to visit when searching for neigbors
-    index.knnSearch(query, indices, dists, nn, flann::SearchParams(128));
-
-    flann::save_to_file(indices,"data/files/result.hdf5","result");
+    int n = index.radiusSearch(query, indices, dists, 3, flann::SearchParams(128));
+    cout << "number of nearest neighbors: " << n << endl;
 
     delete[] dataset.ptr();
     delete[] query.ptr();
